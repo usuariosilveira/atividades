08cd26bd23dedf6f3ecd741f6e6ca1d3cd04c37a
usuariosilveira <moniquesilveiraa28@gmail.com>
2023-10-02 21:33:48 -0300

    primeiro projeto do blog pessoal

---
+++ blogpessoal/src/main/java/com/example/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,27 @@
+package com.example.blogpessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.blogpessoal.model.Postagem;
+import com.example.blogpessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagem")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
+public class PostagemController {
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+
+}
